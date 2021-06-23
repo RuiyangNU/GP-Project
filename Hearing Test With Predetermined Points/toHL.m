c650f03c8837fec9633e3a52bbf4ff4dff09369a
RuiyangNU <raytan2024@u.northwestern.edu>
2021-06-23 14:00:31 -0500

    a

--- Hearing Test With Predetermined Points/toHL.m
+++ Hearing Test With Predetermined Points/toHL.m
@@ -2,10 +2,12 @@
 % Ray Tan, Jeffrey Tang
 % 
 % function hlY = spl2hl(splX, splY, refX, refY)
-% double = double, double, double[], double[]
+% double[] = double[], double[], double[], double[]
 % 
 % Converts a vector of points from dB SPL to HL using a set of reference
-% points. Reference points are defaulted to the points given by the
+% points. If the frequency of a given point in the vector does not match a
+% point in the reference curve, the nearest point in the reference curve is
+% used instead. Reference points are defaulted to the points given by the
 % getRef() function.
 function hl = toHL(freq, spl, refX, refY)
     % set default reference curve
@@ -15,9 +17,9 @@ function hl = toHL(freq, spl, refX, refY)
 
     % locate the index of the frequency value present on the reference
     % curve that is closest to the x value of the given point
-    [~, index] = min(abs(freq - refX));
+    [~, indices] = min(abs(freq - refX'));
     
     % calculate the hearing loss based on the SPL of the reference point
     % and the SPL of the given point
-    hl = spl - refY(index);
+    hl = spl - refY(indices);
 end
