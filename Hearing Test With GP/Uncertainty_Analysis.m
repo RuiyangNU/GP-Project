bfbc64c312174c8aa71b9e2833bbce3a94da66aa
thefinaloneg <69738859+thefinaloneg@users.noreply.github.com>
2021-07-06 15:44:13 -0500

    Update Uncertainty_Analysis.m

--- Hearing Test With GP/Uncertainty_Analysis.m
+++ Hearing Test With GP/Uncertainty_Analysis.m
@@ -1,1 +1,10 @@
-plot(y=120, [20,20000]);
+% initializes the hearing level uncertainty curves
+figure
+x = linspace(hz2bark(125),hz2bark(20000), 1000);
+y = 120*ones(size(x));
+plot(x, y,'g')
+hold on
+
+y = zeros(size(x));
+plot(x, y,'g')
+hold on
