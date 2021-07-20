2ae46bd4b4e5d35614a09933442fdb11399a1797
thefinaloneg <69738859+thefinaloneg@users.noreply.github.com>
2021-07-20 16:38:54 -0500

    Update ninetyfivepercentCurve.m

--- Hearing Test With GP/ninetyfivepercentCurve.m
+++ Hearing Test With GP/ninetyfivepercentCurve.m
@@ -1,1 +1,13 @@
-function curve = ninetyfivepercentCurve(matrix)
+function five, ninetyfive = confidenceCurve(inTest, outTest)
+    noDupes = unique(inTest(:, 1));
+    vals = zeros(1,2);
+    
+    for ii = 1:1:length(noDupes)
+        indicies = find(inTest(:, 1) == noDupes(ii));
+        for jj = 1:1:length(indicies)
+            
+            vals(ii, :) = inTest(indicies(I), :);
+        end
+    end
+end
+noDupes)
