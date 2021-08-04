ee0bb3c6beaaa667acf4d8aa7879a1ee67850cb5
thefinaloneg <69738859+thefinaloneg@users.noreply.github.com>
2021-08-04 12:54:53 -0500

    Update createModulatedStimulus.m

--- Hearing Test With GP/createModulatedStimulus.m
+++ Hearing Test With GP/createModulatedStimulus.m
@@ -1,4 +1,27 @@
-function modulatedStimulus = createModulatedStimulus(amplitude, duration, samplerate, carrierF, modulationF, rampDuration)
-    carrierStimulus = createStimulus(carrierF, amplitude, duration, samplerate);
-    modulatedStimulus = carrierStimulus(linspace(round(samplerate * rampDuration / duration), (end - round(samplerate * rampDuration / duration)), samplerate - 2*round(samplerate * rampDuration / duration))) * modulationF;
+function modulatedStimulus = createModulatedStimulus(carrierF, modulationF, duration, samplerate, rampDuration)
+    if ~exist('duration', 'var') || isempty(duration)
+        duration = 1;
+    end
+    
+    if ~exist('samplerate', 'var') || isempty(samplerate)
+        samplerate = 44100;
+    end
+
+    if ~exist('rampDuration', 'var') || isempty(samplerate)
+        rampDuration = .01;
+    end
+    
+    maxPercent = zeros(1, (1 + duration) * samplerate);
+    
+    % 250ms - 500ms
+    maxPercent(floor(0 * samplerate):floor(rampDuration * samplerate)) = ...
+        linspace(0,1,floor(rampDuration * samplerate) - floor(0 * samplerate) + 1);
+    
+    % 500ms - end of stimulus
+    maxPercent(floor(rampDuration * samplerate):floor(((duration - 4 * rampDuration) / 2)) * samplerate) = 1;
+    
+    % end of stimulus - end of stimulus + 250ms
+    maxPercent(floor((duration - 4 * rampDuration) / 2 * samplerate):floor(duration / 2 * samplerate)) = ...
+        linspace(1,0,floor(duration / 2 * samplerate) - floor(duration - 4 * rampDuration) + 1);
+    
     
