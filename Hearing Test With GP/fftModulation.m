3be5acbd40c13284b76ef4b8ec29bfc57fde92bb
thefinaloneg <69738859+thefinaloneg@users.noreply.github.com>
2021-08-09 10:18:40 -0500

    Update fftModulation.m

--- Hearing Test With GP/fftModulation.m
+++ Hearing Test With GP/fftModulation.m
@@ -1,10 +1,13 @@
-function output = hearOrNot(mouseResponse)
-    fourierResponse = fft(mouseResponse)
-    f1 = max(fourierResponse(1:floor(length(mouseResponse)/2)))
-    f2 = max(fourierResponse(floor(length(mouseResponse)/2)+1:end))
+% returns a "hear" or "no hear" based on the mouse's response to a
+% modulated followed by a carrier frequency using fast fourier transform
+function output = fftModulation(mouseResponse)
+    fourierResponse = fft(mouseResponse);
+% splits the response into the modulated followed by the carrier frequency
+    f1 = max(fourierResponse(1:floor(length(mouseResponse)/2)));
+    f2 = max(fourierResponse(floor(length(mouseResponse)/2)+1:end));
     if f1 > f2 * 1.01;
-        output = "hear"
+        output = "hear";
     else
-        output = "no hear"
+        output = "no hear";
     
     end
