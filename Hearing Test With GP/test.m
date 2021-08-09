c477de1642a0726df42bc9d9537e966ef0138b8a
RuiyangNU <raytan2024@u.northwestern.edu>
2021-08-09 11:28:41 -0500

    Update fftModulation.asv

--- Hearing Test With GP/test.m
+++ Hearing Test With GP/test.m
@@ -1,4 +1,5 @@
 Fs = 44100;
+<<<<<<< Updated upstream
 S = createFM(440, 10, 1);
 L = length(S);
 
@@ -10,3 +11,15 @@ P1(2:end-1) = 2*P1(2:end-1);
 
 f = Fs*(0:L/2)/L;
 plot(f(1:1000), P1(1:1000));
+=======
+S = createAM(100,20,1);
+S = S(1:round(length(S)/2));
+L = length(S);
+
+Y = fft(S);
+P2 = abs(Y/L);
+P1 = 2*P2(1:(L/2+1));
+
+f = Fs *(0:(L/2))/L;
+plot(f(1:500),P1(1:500));
+>>>>>>> Stashed changes
