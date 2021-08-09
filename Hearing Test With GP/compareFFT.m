a346657734b28c6a30caeac4b8eb8c8b3fa7044a
RuiyangNU <raytan2024@u.northwestern.edu>
2021-08-09 11:27:18 -0500

    a

--- Hearing Test With GP/compareFFT.m
+++ Hearing Test With GP/compareFFT.m
@@ -12,12 +12,12 @@ function audible = compareFFT(response)
     modFFT = fft(modResponse);
     modL = length(modResponse);
     modP2 = abs(modFFT/modL);
-    modP1 = modP2(1:modL/2+1);
+    modP1 = modP2(1:ceil(modL/2)+1);
     
     carrFFT = fft(carrResponse);
     carrL = length(carrResponse);
     carrP2 = abs(carrFFT/carrL);
-    carrP1 = carrP2(1:carrL/2+1);
+    carrP1 = carrP2(1:ceil(carrL/2)+1);
     
     % Compare the maxima of the two FFTs and determine whether the
     % modulated response was greater than the carrier response
