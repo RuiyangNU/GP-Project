3d849bcff06a44d75a76d049913d05b16f219979
thefinaloneg <69738859+thefinaloneg@users.noreply.github.com>
2021-08-09 11:05:46 -0500

    Update fftModulation.m

--- Hearing Test With GP/fftModulation.m
+++ Hearing Test With GP/fftModulation.m
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-function output = hearOrNot(mouseResponse)
-    fourierResponse = fft(mouseResponse)
-    f1 = max(fourierResponse(1:floor(length(mouseResponse)/2)))
-    f2 = max(fourierResponse(floor(length(mouseResponse)/2)+1:end))
-    if f1 > f2 * 1.01;
-        output = "hear"
-=======
 % returns a 1 or 0 based on the mouse's response to a
 % modulated followed by a carrier frequency using fast fourier transform
 function output = fftModulation(mouseResponse)
@@ -16,16 +8,13 @@ function output = fftModulation(mouseResponse)
 
     f1 = max(modulatedFreq);
     f2 = max(carrierFreq);
-    % checks if noise 
+    % checks if noise affects the result
     if f1 > f2 * 1.01
         output = 1;
->>>>>>> Stashed changes
     else
-        output = "no hear"
+        output = 0;
     
-<<<<<<< Updated upstream
     end
-=======
     % turns the two sided spectrum into one sided for easier code analysis
     % in the future, does not affect output
     oneSidedModulated = modulatedFreq(1:length(modulatedFreq)/2+1)
@@ -34,4 +23,3 @@ function output = fftModulation(mouseResponse)
     oneSidedCarrier = carrierFreq(1:length(carrierFreq)/2+1)
     oneSidedCarrier(2:end-1) = 2 * carrierFreq(2:end-1)
     
->>>>>>> Stashed changes
